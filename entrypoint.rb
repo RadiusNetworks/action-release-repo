f21ef80edc78a765e161508bc4e873c41c891fcf
Christopher Sexton <csexton@users.noreply.github.com>
2019-10-01 15:12:46 -0400

    Add debugging puts
    To see if the script is running.

--- entrypoint.rb
+++ entrypoint.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'octokit'
 
+puts "Running Release Repo 💫"
 client = Octokit::Client.new(access_token: ENV['GITHUB_API_TOKEN'])
 puts client.user.inspect
 
